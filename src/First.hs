022425f5c9ef3af4fca06c17b9b454e09a8d5d31
yuwa <yuwamama.1213@gmail.com>
2022-06-16 16:16:29 +0900

    add myinit

--- src/First.hs
+++ src/First.hs
@@ -5,3 +5,16 @@ double x = x + x
 
 quadruple :: Int -> Int
 quadruple x = double (double x)
+
+factorial :: Integer -> Integer
+factorial n = product[1 .. n]
+
+average :: [Int] -> Int
+average xs = sum xs `div` length xs
+
+ave :: [Int] -> Int
+ave xs = div (sum xs) (length xs)
+
+myinit :: [a] -> [a]
+myinit (x:[]) = []
+myinit (x:xs) = x : (init xs)
