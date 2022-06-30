f932538d3f4373876e1f21d38660f214d254acad
yuwa <yuwamama.1213@gmail.com>
2022-06-30 16:03:59 +0900

    add mylength

--- src/First.hs
+++ src/First.hs
@@ -18,3 +18,9 @@ ave xs = div (sum xs) (length xs)
 myinit :: [a] -> [a]
 myinit (x:[]) = []
 myinit (x:xs) = x : (init xs)
+
+myconcat :: [[a]] -> [a]
+myconcat xss = [ x | xs <- xss, x <- xs ]
+
+mylength :: [a] -> Int
+mylength xs = sum [ 1 | _ <- xs ]
